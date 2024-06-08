b129917b3dadfb3133656325e1a529a8bab6d2cb
Trieu Vi <luutrieuvi2003@gmail.com>
2024-06-08 13:25:26 +0700

    [Add]_Jwt settings & cors & tokenService_BE

---
+++ Backend/SWD392_GiaSuHocTap/Service/IService/IAuthService.cs
@@ -0,0 +1,15 @@
+﻿using Common.DTO.Auth;
+using DAO.Model;
+
+namespace Service.IService
+{
+    public interface IAuthService
+    {
+        /// <summary>
+        /// Login
+        /// </summary>
+        /// <param name="loginRequest"></param>
+        /// <returns></returns>
+        Task<User> Login(LoginRequestDTO loginRequest);
+    }
+}
---
+++ Backend/SWD392_GiaSuHocTap/Service/IService/ITokenService.cs
@@ -0,0 +1,15 @@
+﻿using DAO.Model;
+
+namespace Service.IService
+{
+    public interface ITokenService
+    {
+        /// <summary>
+        /// Create JWT Token
+        /// </summary>
+        /// <param name="user"></param>
+        /// <param name="role"></param>
+        /// <returns></returns>
+        string CreateJWTToken(User user, string role);
+    }
+}
