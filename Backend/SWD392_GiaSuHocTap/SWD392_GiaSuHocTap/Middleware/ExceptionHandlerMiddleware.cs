6bfecd80b34fa9df0f8f3362ac15f6432844b611
Trieu Vi <luutrieuvi2003@gmail.com>
2024-06-09 12:39:05 +0700

    [Add]_Global handling exception_BE

---
+++ Backend/SWD392_GiaSuHocTap/SWD392_GiaSuHocTap/Middleware/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,41 @@
+﻿using Common.DTO;
+using System.Net;
+
+namespace SWD392_GiaSuHocTap.Middleware
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionHandlerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context) 
+        {
+            try
+            {
+                await _next(context);
+            } catch (Exception ex)
+            {
+                await HandleError(context, ex);
+            }
+        }
+
+        private static async Task HandleError(HttpContext context, Exception ex)
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            context.Response.ContentType = "application/json";
+
+            var response = new ResponseDTO()
+            {
+                StatusCode = (int)HttpStatusCode.InternalServerError,
+                Message = ex.Message,
+                Data = null
+            };
+
+            await context.Response.WriteAsJsonAsync(response);
+        }
+    }
+}
