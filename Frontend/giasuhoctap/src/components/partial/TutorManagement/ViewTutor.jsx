c795c65627c0778d29a03043a48aa930bdbf8f15
leviethung220703@gmail.com <leviethung220703@gmail.com>
2024-06-14 16:57:19 +0700

    [Update]_Update UI for table_FE

--- Frontend/giasuhoctap/src/components/partial/TutorManagement/ViewTutor.jsx
+++ Frontend/giasuhoctap/src/components/partial/TutorManagement/ViewTutor.jsx
@@ -1,21 +1,29 @@
-import React from 'react'
-import Container from './Container'
-import Search_Input from '../../global/Search_Input'
-import Header from './Header'
-import Body from './Body'
-import TableList from './TableList'
+import React from "react";
+import Container from "./Container";
+import Search_Input from "../../global/Search_Input";
+import Header from "./Header";
+import Body from "./Body";
+import TableList from "./TableList";
+import Title from "./Title";
 
 export default function ViewTutor() {
-    return (
-        <div>
-            <Container>
-                <Header>
-                    <Search_Input />
-                </Header>
-                <Body>
-                    <TableList/>
-                </Body>
-            </Container>
-        </div>
-    )
+  return (
+    <div
+      style={{
+        padding: "25px",
+        borderRadius: "10px",
+        boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px",
+      }}
+    >
+      <Container>
+        <Title title="List of Tutors" />
+        <Header>
+          <Search_Input />
+        </Header>
+        <Body>
+          <TableList />
+        </Body>
+      </Container>
+    </div>
+  );
 }
