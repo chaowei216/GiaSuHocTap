775ec957698a55d2a2ed0793a26b41a7e5ffa732
Nam Ng Phuong <npnam03@gmail.com>
2024-06-15 15:35:38 +0700

    [Fix]_dto and status code_BE

---
+++ Backend/SWD392_GiaSuHocTap/Common/DTO/Auth/EmailDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Common.DTO.Auth
+{
+    public class EmailDTO
+    {
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+    }
+}
