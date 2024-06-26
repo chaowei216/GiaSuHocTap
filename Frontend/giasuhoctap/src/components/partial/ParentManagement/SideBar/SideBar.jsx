37f54bc875c8debf505e5447aabd06ce2053983f
lucnm <lucnm007@gmail.com>
2024-06-26 23:17:11 +0700

    [Update]_Parent Page_FE

--- Frontend/giasuhoctap/src/components/partial/ParentManagement/SideBar/SideBar.jsx
+++ Frontend/giasuhoctap/src/components/partial/ParentManagement/SideBar/SideBar.jsx
@@ -1,68 +1,52 @@
 import React, { useState } from 'react';
 import styles from './SideBar.module.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'boxicons/css/boxicons.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoneyBillWave, faIdCard } from '@fortawesome/free-solid-svg-icons';
 
 const SideBar = () => {
-  const [showNav, setShowNav] = useState(false);
+  const [showAccount, setShowAccount] = useState(false);
+  const [showWallet, setShowWallet] = useState(false);
 
-  const toggleNav = () => {
-    setShowNav(!showNav);
-  };
+  const toggleAccountDropdown = () => setShowAccount(!showAccount);
+  const toggleWalletDropdown = () => setShowWallet(!showWallet);
 
   return (
-    <div className={`${styles.body} ${showNav ? styles.body_pd : ''}`}>
-      <header className={`${styles.header} ${showNav ? styles.body_pd : ''}`} id="header">
-        <div className={styles.header_toggle} onClick={toggleNav}>
-          <i className={`bx ${showNav ? 'bx-x' : 'bx-menu'}`} id="header-toggle"></i>
-        </div>
-        <div className={styles.header_img}>
-          <img src="https://i.imgur.com/hczKIze.jpg" alt="Profile" />
-        </div>
-      </header>
-      <div className={`${styles.l_navbar} ${showNav ? styles.show : ''}`} id="nav-bar">
-        <nav className={styles.nav}>
-          <div>
-            <a href="#" className={`${styles.nav_logo} nav_logo`}>
-              <i className='bx bx-layer nav_logo-icon'></i>
-              <span className={styles.nav_logo_name}>BBBootstrap</span>
-            </a>
-            <div className={styles.nav_list}>
-              <a href="#" className={`${styles.nav_link} ${styles.active}`}>
-                <i className='bx bx-grid-alt nav_icon'></i>
-                <span className={styles.nav_name}>Dashboard</span>
-              </a>
-              <a href="#" className={styles.nav_link}>
-                <i className='bx bx-user nav_icon'></i>
-                <span className={styles.nav_name}>Users</span>
-              </a>
-              <a href="#" className={styles.nav_link}>
-                <i className='bx bx-message-square-detail nav_icon'></i>
-                <span className={styles.nav_name}>Messages</span>
-              </a>
-              <a href="#" className={styles.nav_link}>
-                <i className='bx bx-bookmark nav_icon'></i>
-                <span className={styles.nav_name}>Bookmark</span>
-              </a>
-              <a href="#" className={styles.nav_link}>
-                <i className='bx bx-folder nav_icon'></i>
-                <span className={styles.nav_name}>Files</span>
-              </a>
-              <a href="#" className={styles.nav_link}>
-                <i className='bx bx-bar-chart-alt-2 nav_icon'></i>
-                <span className={styles.nav_name}>Stats</span>
-              </a>
+    <div className={styles.sidebar}>
+      <nav className={styles.nav}>
+        <div className={styles.nav_list}>
+          <div className={styles.nav_item}>
+            <div className={styles.nav_link} onClick={toggleAccountDropdown}>
+              <FontAwesomeIcon icon={faIdCard} className={styles.icon} />
+              <span className={styles.nav_name}>TÀI KHOẢN</span>
             </div>
+            {showAccount && (
+              <div className={styles.dropdown_menu}>
+                <a className={styles.dropdown_item} href="/ParentPage">Thông tin cá nhân</a>
+                <a className={styles.dropdown_item} href="#/action-2">Theo dõi Parent</a>
+                <a className={styles.dropdown_item} href="#/action-3">Lịch sử giao dịch</a>
+                <a className={styles.dropdown_item} href="#/action-3">Thanh toán</a>
+                <a className={styles.dropdown_item} href="#/action-3">Ví</a>
+              </div>
+            )}
           </div>
-          <a href="#" className={styles.nav_link}>
-            <i className='bx bx-log-out nav_icon'></i>
-            <span className={styles.nav_name}>SignOut</span>
-          </a>
-        </nav>
-      </div>
-      <div className="height-100 bg-light">
-        <h4>Main Components</h4>
-      </div>
+          <hr style={{width: '95%', color: '#000000b5'}}/>
+          <div className={styles.nav_item}>
+            <div className={styles.nav_link} onClick={toggleWalletDropdown}>
+              <FontAwesomeIcon icon={faMoneyBillWave} className={styles.icon} />
+              <span className={styles.nav_name}>VÍ ĐIỆN TỬ</span>
+            </div>
+            {showWallet && (
+              <div className={styles.dropdown_menu}>
+                <a className={styles.dropdown_item} href="#/action-1">Option 1</a>
+                <a className={styles.dropdown_item} href="#/action-2">Option 2</a>
+                <a className={styles.dropdown_item} href="#/action-3">Option 3</a>
+              </div>
+            )}
+          </div>
+          <hr style={{width: '90%', color: '#000000b5'}}/>
+        </div>
+      </nav>
     </div>
   );
 };
--- Frontend/giasuhoctap/src/pages/ParentPage/ParentPage.jsx
+++ Frontend/giasuhoctap/src/pages/ParentPage/ParentPage.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import SideBar from '../../components/partial/ParentManagement/SideBar/SideBar'
-
-
+import ParentProfile from '../../components/partial/ParentManagement/ParentProfile/ParentProfile'
+import FlowAccount from '../../components/partial/ParentManagement/FlowAccount/FlowAccount'
 
 export default function ParentPage() {
     return (
-        <div>
-            <SideBar />
+        <div style={{ display: 'flex', height: '100%', width: '100%' }}>
+            <div style={{ width: '20%' }}>
+                <SideBar />
+            </div>
+            <div style={{ width: '80%' }}>
+                <div>
+                    <FlowAccount />
+                    <ParentProfile />
+                </div>
+
+            </div>
         </div>
     )
 }
