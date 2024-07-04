17e4cf2e7a19a2e3e953d6b18834df5690a67834
lucnm <lucnm007@gmail.com>
2024-07-04 15:11:51 +0700

    [Add]_Resigter Tutor Api_FE

--- Frontend/giasuhoctap/src/components/partial/ParentManagement/ParentProfile/ValidationParentProfile.jsx
+++ Frontend/giasuhoctap/src/components/partial/ParentManagement/ParentProfile/ValidationParentProfile.jsx
@@ -3,7 +3,7 @@ import * as Yup from 'yup';
 export const validationSchema = Yup.object().shape({
     fullName: Yup.string()
         .required('* Họ và tên là bắt buộc'),
-    phone: Yup.string()
+        phone: Yup.string()
         .required("Vui lòng nhập số điện thoại")
         .min(10, "Số điện thoại ít nhất 10 số")
         .max(11, "Số điện thoại nhiều nhất 11 số") // Sửa từ "10" thành "11"
@@ -16,7 +16,7 @@ export const validationSchema = Yup.object().shape({
         .required('* Ngày sinh là bắt buộc'),
     city: Yup.string()
         .required('* Thành phố là bắt buộc'),
-    district: Yup.string()
+        district: Yup.string()
         .required('* Quận/ Huyện là bắt buộc'),
     address: Yup.string()
         .min(5, "Địa chỉ có ít nhất 5 ký tự")
