255c87adb6903085f970d91c8b50ccd35b17239f
Nam Ng Phuong <npnam03@gmail.com>
2024-07-08 01:01:49 +0700

    [Add]_get feedback api_BE

--- Backend/SWD392_GiaSuHocTap/SWD392_GiaSuHocTap/Controllers/FeedbackController.cs
+++ Backend/SWD392_GiaSuHocTap/SWD392_GiaSuHocTap/Controllers/FeedbackController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Service.IService;
 using Service.Service;
 using Common.DTO.Feedback;
+using Common.DTO.Query;
 
 namespace SWD392_GiaSuHocTap.Controllers
 {
@@ -15,11 +16,49 @@ namespace SWD392_GiaSuHocTap.Controllers
     public class FeedbackController : ControllerBase
     {
         private readonly IFeedbackService _feedbackService;
+        private readonly IUserService _userService;
 
-        public FeedbackController(IFeedbackService feedbackService)
+        public FeedbackController(IFeedbackService feedbackService, IUserService userService)
         {
             _feedbackService = feedbackService;
+            _userService = userService;
         }
+
+        [HttpGet("get-feedbacks-of-tutors/{tutorId}")]
+        public async Task<IActionResult> GetOfflineRequestsOfTutor(int tutorId, [FromQuery] FeedbackParameters parameters)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new ResponseDTO()
+                {
+                    StatusCode = (int)StatusCodeEnum.BadRequest,
+                    Message = ModelState.ToString()!,
+                    Data = null
+                }); 
+            }
+
+            var tutor = await _userService.GetUserById(tutorId);
+
+            if (tutor != null)
+            {
+                var response = _feedbackService.GetFeedbacksOfTutor(tutorId, parameters);
+
+                return Ok(new ResponseDTO()
+                {
+                    StatusCode = (int)StatusCodeEnum.OK,
+                    Message = GeneralMessage.Success,
+                    Data = response
+                });
+            }
+
+            return StatusCode(404, new ResponseDTO()
+            {
+                StatusCode = (int)StatusCodeEnum.NotFound,
+                Message = GeneralMessage.NotFound,
+                Data = null
+            });
+        }
+
 
         [HttpPost("create-feedback")]
         public async Task<IActionResult> CreateSystemNotification([FromBody] FeedbackCreateDTO feedbackInfo)
