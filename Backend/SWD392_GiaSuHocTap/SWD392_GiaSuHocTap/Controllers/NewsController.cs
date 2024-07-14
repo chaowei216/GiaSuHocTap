b176a928b1ed6175ea92d87f9500db8fff9e6ae3
Luu Trieu Vi <luutrieuvi2003@gmail.com>
2024-07-14 13:29:30 +0700

    [Update]_Update API news_BE

--- Backend/SWD392_GiaSuHocTap/SWD392_GiaSuHocTap/Controllers/NewsController.cs
+++ Backend/SWD392_GiaSuHocTap/SWD392_GiaSuHocTap/Controllers/NewsController.cs
@@ -23,7 +23,7 @@ namespace SWD392_GiaSuHocTap.Controllers
         }
 
         [HttpGet("get-news-paging")]
-        public async Task<IActionResult> GetNewsPaging([FromQuery] NewsParameters parameters)
+        public IActionResult GetNewsPaging([FromQuery] NewsParameters parameters)
         {
             if (!ModelState.IsValid)
             {
@@ -39,7 +39,7 @@ namespace SWD392_GiaSuHocTap.Controllers
 
             return Ok(new ResponseDTO()
             {
-                StatusCode = (int)StatusCodeEnum.Created,
+                StatusCode = (int)StatusCodeEnum.OK,
                 Message = GeneralMessage.Success,
                 Data = response
             });
@@ -74,7 +74,7 @@ namespace SWD392_GiaSuHocTap.Controllers
             {
                 return Ok(new ResponseDTO()
                 {
-                    StatusCode = (int)StatusCodeEnum.OK,
+                    StatusCode = (int)StatusCodeEnum.Created,
                     Message = GeneralMessage.Success,
                     Data = feedback
                 });
